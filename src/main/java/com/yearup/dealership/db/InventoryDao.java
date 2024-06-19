85f12c70f82b6d23eff151d1c17d8110ab26f8d9
alexis-renee <166551493+alexis-renee@users.noreply.github.com>
2024-06-19 19:07:39 -0400

    Updated commit

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -13,10 +13,48 @@ public class InventoryDao {
     }
 
     public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+       String sql = "INSERT INTO inventory (vin, dealership_id) VALUES (?, ?)";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, vin);
+            preparedStatement.setInt(2, dealershipId);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Vehicle added to inventory successfully.");
+            } else {
+                System.out.println("Failed to add vehicle to inventory.");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error adding vehicle to inventory: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
-        // TODO: Implement the logic to remove a vehicle from the inventory
+        String sql = "DELETE FROM inventory WHERE = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, vin);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Vehicle deleted from inventory VIN does not exist.");
+            } else {
+                System.out.println("Failed to delete vehicle to inventory.");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error deleting vehicle from inventory: " + e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 }
